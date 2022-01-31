07273660529b7ac46ea1ed73ed80d30e47fd8087
mk-pmb <mk-pmb@users.noreply.github.com>
2022-01-31 02:04:01 +0100

    Report date mismatch in easier wording

--- mongo2pg/m-anno/verifyLatestOldRevi.mjs
+++ mongo2pg/m-anno/verifyLatestOldRevi.mjs
@@ -1,18 +1,30 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import equal from 'equal-pmb';
+import msecToHumanDuration from 'pretty-ms';
+
+
+function dateDump(raw, parsed) {
+  return (JSON.stringify(raw) + ', parsed: ' + parsed);
+}
 
 
 const veri = function verifyLatestOldRevi(meta, topDataRevi, latestOldRevi) {
   if (!latestOldRevi) { return; }
   const metaMod = meta.dateLastModif;
+  const latestCreatedOrig = latestOldRevi.created;
 
-  if (metaMod !== latestOldRevi.created) {
-    const msg = ('Dates mismatch:'
-      + '\nTop-level anno was last modified '
-      + JSON.stringify(metaMod)
-      + '\nbut latest revision was created  '
-      + JSON.stringify(latestOldRevi.created));
+  if (metaMod !== latestCreatedOrig) {
+    const dateModif = new Date(metaMod);
+    const dateCreated = new Date(latestCreatedOrig);
+    const modifDeltaMsec = (dateModif - dateCreated);
+    const modifAfterCreated = (modifDeltaMsec >= 0);
+    const msg = ('Dates mismatch: Latest revision was'
+      + '\ncreated  ' + dateDump(latestCreatedOrig, dateCreated)
+      + ' but parent anno was last'
+      + '\nmodified ' + dateDump(metaMod, dateModif)
+      + ', i.e. ' + msecToHumanDuration(Math.abs(modifDeltaMsec))
+      + ' ' + (modifAfterCreated ? 'after' : 'BEFORE') + ' creation.');
     throw new Error(msg);
   }
 
