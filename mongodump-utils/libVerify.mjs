4d40de3cce237f2e2a2509a039ea8bbbc5774d44
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-09 02:18:15 +0100

    assumedCreatorAliases

--- mongodump-utils/libVerify.mjs
+++ mongodump-utils/libVerify.mjs
@@ -142,7 +142,15 @@ const veri = {
     const cas = job.creatorAliases;
     if (cas && ex && ex.id && ac && ac.id) {
       if (ac.id === ex.id) { return; }
-      const alias = getOwn(cas, ac.id);
+      let alias = getOwn(cas, ac.id);
+      if (!alias) {
+        const learn = job.hint('assumedCreatorAliases', undefined, {});
+        alias = getOwn(learn, ac.id);
+        if (!alias) {
+          alias = ex.id;
+          learn[ac.id] = alias;
+        }
+      }
       if (alias && (ex.id === alias)) {
         job.counters.add('usingCreatorAlias');
         job.counters.add('usingCreatorAlias:' + alias);
