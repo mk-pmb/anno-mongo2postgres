823fa7ca9491754e0a369572fc2bb3237184bc56
mk-pmb <mk-pmb@users.noreply.github.com>
2022-06-30 03:02:17 +0200

    v0.1.1: Factor out guessSubjectTarget

--- src/guessSubjectTarget.mjs
+++ src/guessSubjectTarget.mjs
@@ -1,47 +1,6 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import mustBe from 'typechecks-pmb/must-be';
+import guessAndParse
+  from 'webanno-guess-subject-target-url-pmb/extra/parse.mjs';
 
-const safeUrlRgx = /^https?:\/{2}([a-z0-9_:\.\/\-]+)$/;
-
-const EX = function guessSubjectTarget(anno) {
-  let tgt = anno.target;
-  if (Array.isArray(tgt)) {
-    const n = tgt.length;
-    if (n < 1) { throw new Error('Found no target'); }
-    if (n > 1) { throw new Error('Found too many targets'); }
-    [tgt] = tgt;
-  }
-  tgt = (tgt.scope
-    || tgt.id
-    || tgt.source
-    || tgt);
-  mustBe.nest('Target URL', tgt);
-  const um = safeUrlRgx.exec(tgt.toLowerCase());
-  if (!um) { throw new Error('Unsupported target URL format: ' + tgt); }
-  const pathParts = tgt.split(/\/+/).filter(Boolean).slice(1);
-  let port = 0;
-  const host = pathParts.shift().replace(/:\d+$/, function hasPort(m) {
-    port = +m.slice(1);
-    return '';
-  });
-  const revHost = host.split('.').reverse().join('.');
-  return {
-    url: tgt,
-    host,
-    port,
-    revHost,
-    pathParts,
-  };
-};
-
-
-
-
-
-
-
-
-
-
-export default EX;
+export default guessAndParse;
