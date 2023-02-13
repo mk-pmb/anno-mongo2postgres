76f5fa81c839941485bcebd3586a688ebd99c1df
mk-pmb <mk-pmb@users.noreply.github.com>
2023-02-13 02:52:44 +0100

    Use date and time as success message.

--- by-target/lrl.sh
+++ by-target/lrl.sh
@@ -73,7 +73,7 @@ function concat_sql_files () {
   done >"$DEST" || return $?
   printf '%s\n' \
     '-- pgAdminer needs a statement (i.e. not comment) at end of file:' \
-    'SELECT 1 AS "success";' \
+    "SELECT TO_CHAR(NOW(), 'YYYY-MM-DD HH24:MI:SS')"' AS "success";' \
     >>"$DEST" || return $?
 }
 
