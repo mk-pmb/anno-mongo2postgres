0433615a2656355179f2df8c6bd9f7fe181c06f7
mk-pmb <mk-pmb@users.noreply.github.com>
2023-06-26 02:26:23 +0200

    v0.1.6: Move SQL writer into pgDumpWriter.

--- src/convertDissectedAnnos/fmtInserts.mjs
+++ src/convertDissectedAnnos/fmtInserts.mjs
@@ -1,7 +1,5 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import nodeFs from 'fs';
-
 import mustBe from 'typechecks-pmb/must-be.js';
 import objMapValues from 'lodash.mapvalues';
 import objPop from 'objpop';
@@ -10,53 +8,35 @@ import pgDumpWriter from 'postgres-dump-writer-helpers-220524-pmb';
 import verify from '../verifyAnno/index.mjs';
 
 
-const sqlFiles = new Map(/* table name -> file stream */);
-sqlFiles.writeRec = function writeSql(common, rec) {
-  const tbl = rec.TABLE;
-  let pgStream = sqlFiles.get(tbl);
-  if (!pgStream) {
-    const destFilePath = 'tmp.pg.' + tbl + '.sql';
-    const fileStream = nodeFs.createWriteStream(destFilePath);
-    pgStream = pgDumpWriter.stmtStream.fromNativeWriteStream(fileStream);
-    sqlFiles.set(tbl, pgStream);
-  }
-  pgDumpWriter.fmtInsert(common, rec, { STREAM: pgStream });
-};
+const sqlWriter = pgDumpWriter.makeFileMappedSqlWriter({
+  destFilePathTemplate: ['tmp.pg.', '.sql'],
+});
 
 
 const EX = function fmtInserts(anno, auxMeta) {
   const auxPop = objPop(auxMeta, { mustBe }).mustBe;
   const reviNum = auxPop('pos int', 'reviNum').toFixed(0);
   verify.reviUrl(anno, 'id', { reviNum });
-  const writeRec = sqlFiles.writeRec.bind(null, {
+  const idParts = {
     anno_id: anno.divePath.expectedContainerAnnoId,
     revision_id: reviNum,
-  });
+  };
 
-  // anno data record
-  const adRec = {
+  sqlWriter.writeRec({
     TABLE: 'anno_data',
+    ...idParts,
     ...anno.meta,
     details: anno.data,
-  };
-  writeRec(adRec);
+  });
 
   objMapValues(anno.relations, function declare(url, rel) {
-    const linkRec = { TABLE: 'anno_links', rel, url };
-    writeRec(linkRec);
+    sqlWriter.writeRec({ TABLE: 'anno_links', ...idParts, rel, url });
   });
 };
 
 
 Object.assign(EX, {
-
-  getFilesMap() { return sqlFiles; },
-
-  endAll() {
-    sqlFiles.forEach(pgStream => pgStream.end());
-    sqlFiles.clear();
-  },
-
+  endAll: sqlWriter.endAll,
 });
 
 
