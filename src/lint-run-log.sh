0a99eec2e7ec099cb15be82a6cfb438a793b96db
mk-pmb <mk-pmb@users.noreply.github.com>
2024-04-08 21:27:25 +0200

    Fix linting.

--- src/lint-run-log.sh
+++ src/lint-run-log.sh
@@ -18,17 +18,21 @@ function lint_and_run () {
 
 
   local TMP_BFN="$SELFPATH/tmp.${BFN//\//.}"
+  local UNBUF_TEE='exec stdbuf -i0 -o0 -e0 tee --'
   nodemjs "$MJS" "$@" \
-    2> >(unbuffered tee -- "$TMP_BFN.err" >&2) \
-    > >(unbuffered tee -- "$TMP_BFN.txt") \
+    2> >($UNBUF_TEE "$TMP_BFN.err" >&2) \
+    > >($UNBUF_TEE "$TMP_BFN.txt") \
     || return $?
 }
 
 
 function just_lint () {
   pushd -- "$SELFPATH" >/dev/null || return $?
-  if which elp | grep -qFe '^/'; then elp || return $?; fi
-  eslint --ext=js,mjs . && echo '+OK eslint succeeded.' || return $?
+  if which elp | grep -qPe '^/'; then
+    elp || return $?
+  else
+    npm run lint || return $?
+  fi
   popd >/dev/null || return $?
 }
 
