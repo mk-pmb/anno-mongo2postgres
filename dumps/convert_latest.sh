6d5c028e0aa6a987ae5b957fd7385a0fe892ab47
mk-pmb <mk-pmb@users.noreply.github.com>
2024-07-02 23:14:06 +0200

    convert_latest: No longer pu by default.

--- dumps/convert_latest.sh
+++ dumps/convert_latest.sh
@@ -7,7 +7,6 @@ sudo -Eu "$(stat -c %U .)" sh -c 'export TRAFO_MAXERR=100 SKIP_LINT=y; (
     && npm run users \
     && npm run lrl dis \
     && npm run lrl pg \
-    && npm run pu \
     && echo
   echo "started: $ST. REWRITE_BASEURL was ‹$REWRITE_BASEURL›"
   echo "done:    $(date -R)"
