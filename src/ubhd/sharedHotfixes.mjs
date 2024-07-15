296f5a6cb709528fd3dd8bab3b54017a596ab9b7
mk-pmb <mk-pmb@users.noreply.github.com>
2024-07-15 20:14:01 +0200

    Skip another test anno.

--- src/ubhd/sharedHotfixes.mjs
+++ src/ubhd/sharedHotfixes.mjs
@@ -7,6 +7,7 @@ const shf = {
     s.add('787d953050996');
     s.add('cb219db7cbb20');
     s.add('CRo8W4H2RpW4l4hHcofuVQ');
+    s.add('UL9bSMsbQwu8rBaZO5HrqA');
     s.add('X2ITI56MRp-viA-H9jVV2g');
   },
 
