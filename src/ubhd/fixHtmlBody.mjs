f704ec0078aa57efacb4fbbc3917a6afe3aee343
mk-pmb <mk-pmb@users.noreply.github.com>
2024-11-18 19:51:38 +0100

    Adapt to new HTML sanitizer version.

--- src/ubhd/fixHtmlBody.mjs
+++ src/ubhd/fixHtmlBody.mjs
@@ -39,6 +39,8 @@ EX.core = function core(trace, origHtml) {
   h = h.replace(/>\s*(<(?:p|div))(?=\s|>)/g, '>\n$1');
   h = h.replace(/(<\/(?:p|div)>)\s*/g, '$1\n');
   h = h.replace(/\s*(<br[ \/]*>)\s*/g, '$1\n');
+  h = h.replace(/(?:<br[ \/]*>|\s)+(<\/p>)\s*$/g, '$1');
+  h = h.replace(/(?:<p>\s*<\/p>|\s)+$/g, '');
   h = h.trim();
   return h;
 };
